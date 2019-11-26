aa6cc4297ca34a06f3c642bfa6457e43d25204dd
dpasc <damianpascale@outlook.com>
2019-11-26 12:10:19 +1100

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {HttpClient} from "@angular/common/http"
+import { ObjectUnsubscribedError } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,16 +10,26 @@ import {HttpClient} from "@angular/common/http"
 export class AppComponent {
   title = 'GTAng';
   private data:any = []
+  travelPackageid: number
   city =""
   constructor(private http: HttpClient){}
 
 
   getData(){
-    const url = 'https://localhost:44376/api/travelpackage?city=';
+    const url = 'https://localhost:44376/api/travelpackage?city='
     this.http.get(url+this.city).subscribe((res)=>
     {
       this.data=res
       console.log(this.data)
+
     })
   }
+
+  getKeys(input){
+    return Object.keys(input)
+
+  }
+
 }
+
+//'https://localhost:44376/api/travelpackage?city=';
